6442bbf222991e4309df45e2c4b6dbce0c08c89a
KEY4\vetal <dubrovskiy.vitaliy@key4.com.ua>
2017-08-22 19:30:05 +0300

    no message

--- git.sh
+++ git.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 REMOTE=$(git ls-remote https://github.com/key4-nginx/nginx_config.git | grep HEAD | awk '{ print $1}')
 LOCAL=$(git rev-parse @)
-#echo $REMOTE
-#echo $LOCAL
 if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
-#elif [ $LOCAL != $REMOTE ]; then
-#    echo "Need to pull"
+elif [ $LOCAL != $REMOTE ]; then
+    echo "Need to pull"
 else
-    echo "fwafw"
+    echo "Diverged"
+fi
